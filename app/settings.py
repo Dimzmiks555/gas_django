fc706b11ab4cfa7fd95b5429f0671714a1f15f6e
Your Name <you@example.com>
2024-07-01 23:29:15 +0300

    test

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ PWA_SERVICE_WORKER_PATH = BASE_DIR / 'gas/static' / 'serviceworker.js'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://*']
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok-free.app']
 
 
 
@@ -143,20 +143,23 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': 'static/images/icon-160x160.png',
-        'sizes': '160x160'
+        'src': 'static/images/i.png',
+        'sizes': '160x160',
+        "purpose": "any"
     }
 ]
 PWA_APP_ICONS_APPLE = [
     {
-        'src': 'static/images/icon-160x160.png',
-        'sizes': '160x160'
+        'src': 'static/images/i.png',
+        'sizes': '160x160',
+        "purpose": "any"
     }
 ]
 PWA_APP_SPLASH_SCREEN = [
     {
         'src': 'static/images/icon.png',
-        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+        "purpose": "any"
     }
 ]
 PWA_APP_DIR = 'ltr'
